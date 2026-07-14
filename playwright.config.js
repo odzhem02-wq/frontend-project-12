3df8cc186b2be65b019aa1f06673dfdc515fa407
dzhemdzhem <o.dzhem02@mail.ru>
2026-07-14 14:28:16 +0300

    fix: remove playwright.config.js

--- playwright.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@playwright/test';
-
-export default defineConfig({
-  webServer: {
-    command: 'make start',
-    port: 5001,
-    reuseExistingServer: !process.env.CI,
-  },
-});
